72cc1c0ac2cb696e07a47897eca23f993fb76c74
EsraaAnwerIsmail <esraaanwer2002@gmail.com>
2025-11-02 01:53:48 +0200

    Create dynamic odoo.conf to fix postgres user issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -e
 
-# السماح باستخدام postgres user
-export ALLOW_ADMIN_USER=True
+# كتابة ملف odoo.conf ديناميكياً
+cat > /etc/odoo/odoo.conf << EOF
+[options]
+addons_path = /mnt/extra-addons,/usr/lib/python3/dist-packages/odoo/addons
+admin_passwd = ${ADMIN_PASSWD:-admin}
+db_host = ${PGHOST}
+db_port = ${PGPORT:-5432}
+db_user = ${PGUSER}
+db_password = ${PGPASSWORD}
+db_name = False
+http_port = 8069
+proxy_mode = True
+db_maxconn = 64
+limit_time_cpu = 600
+limit_time_real = 1200
+list_db = True
+EOF
 
-# تشغيل Odoo مع المتغيرات البيئية
-exec odoo \
-  --addons-path=/mnt/extra-addons,/usr/lib/python3/dist-packages/odoo/addons \
-  --db_host=${PGHOST} \
-  --db_port=${PGPORT:-5432} \
-  --db_user=${PGUSER} \
-  --db_password=${PGPASSWORD} \
-  --http-port=8069 \
-  --proxy-mode \
-  --no-database-list
+# تشغيل Odoo
+exec odoo -c /etc/odoo/odoo.conf
